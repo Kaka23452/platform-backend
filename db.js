dcbb81bc0c0d229f73b7dc1f873f75864e8bb8b3
Kaka23452 <hooktgol@atomicmail.io>
2026-06-14 12:14:58 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "postgresql",
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || "sampledb",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
