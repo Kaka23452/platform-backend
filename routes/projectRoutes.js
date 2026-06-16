82d41c1f60eb6413edb894711259c34e3beedaca
Kaka23452 <hooktgol@atomicmail.io>
2026-06-16 09:53:16 +0530

    Update projectRoutes.js

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,1 +1,13 @@
+const express = require('express');
 
+const router = express.Router();
+
+const {
+  getProjects,
+  createProject
+} = require('../controllers/projectController');
+
+router.get('/', getProjects);
+router.post('/', createProject);
+
+module.exports = router;
