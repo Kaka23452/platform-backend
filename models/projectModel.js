e90dc6a553ca3849143510fcdf0d4c0b452d44a3
Kaka23452 <hooktgol@atomicmail.io>
2026-06-16 09:32:47 +0530

    Update projectModel.js

--- models/projectModel.js
+++ models/projectModel.js
@@ -1,22 +1,31 @@
 const pool = require('../db');
 
-async function getAllProjects() {
+async function getUserByEmail(email) {
   const result = await pool.query(
-    'SELECT * FROM projects ORDER BY id DESC'
+    'SELECT * FROM users WHERE email = $1',
+    [email]
   );
-  return result.rows;
+
+  return result.rows[0];
 }
 
-async function createProject(name, description) {
+async function createUser(
+  email,
+  username,
+  passwordHash
+) {
   const result = await pool.query(
-    'INSERT INTO projects(name, description) VALUES($1, $2) RETURNING *',
-    [name, description]
+    `INSERT INTO users
+     (email, username, password_hash)
+     VALUES ($1, $2, $3)
+     RETURNING *`,
+    [email, username, passwordHash]
   );
 
   return result.rows[0];
 }
 
 module.exports = {
-  getAllProjects,
-  createProject
+  getUserByEmail,
+  createUser
 };
