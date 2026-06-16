0f2010b9d4092fda74f8e7d9e53460b9d05f53c3
Kaka23452 <hooktgol@atomicmail.io>
2026-06-16 09:42:57 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,3 +9,12 @@ const {
 router.post('/register', register);
 
 module.exports = router;
+
+
+const projectRoutes = require("./routes/projectRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+...
+
+app.use("/api/projects", projectRoutes);
+app.use("/api/auth", authRoutes);
